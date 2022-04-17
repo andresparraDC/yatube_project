2c9f6422d1d18c01919ed5d542548c5eb1c51809
AndresParra <freddyandresparraorellana@yandex.com>
2022-04-18 00:47:56 +0300

    Commit 4: All is OK. Se aplica render para acceder al index.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('This is my HOME Saphiens!')
+    # return HttpResponse('This is my HOME Saphiens!')
+
+    # html_content = '<html><head><title>Yatube</title></head><body>'
+    # html_content += '<h1>Главная страница</h1>'
+    # html_content += '</body></html>'
+    # return HttpResponse(html_content)
+
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, pk):
